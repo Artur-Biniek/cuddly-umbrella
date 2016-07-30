eb75b175c8bdb82fe014b4b0d583d411e687352f
Artur-Biniek <artur.biniek@gmail.com>
2016-07-30 09:05:05 +0100

    Lexing of commas, colons, semicolons and parens.

--- src/CuddlyUmbrella.Compiler.Tests/LexerTests.fs
+++ src/CuddlyUmbrella.Compiler.Tests/LexerTests.fs
@@ -6,21 +6,71 @@ open CuddlyUmbrella.Compiler
 open Parser
 
 [<Test>]
-[<TestCaseSource("testCases")>]
-let ``should tokenize identifiers correctly`` input =
+[<TestCaseSource("correctIdentifierTestCases")>]
+let ``tokenizes identifiers correctly`` input =
     let lexbuf = LexBuffer<_>.FromString input
     let tokens = Lexer.getAllTokens lexbuf
     tokens
 
-let testCases =
-    ["id",
-        [IDENT("id"); EOF];
+[<Test>]
+let ``ignores spaces, tabs, and line endings``() =
+    let lexbuf = LexBuffer<_>.FromString "             \t   \t  \t   \t\t\r\n\t   \n"
+    let tokens = Lexer.getAllTokens lexbuf
+    let areEqual = (tokens = [EOF]);
+    Assert.That(areEqual)
 
-     "ab cdef",
-        [IDENT("ab"); IDENT("cdef"); EOF];
+[<Test>]
+[<TestCaseSource("correctPunctuationsTestCases")>]
+let ``tokenizes punctuations correctly`` input =
+    let lexbuf = LexBuffer<_>.FromString input
+    let tokens = Lexer.getAllTokens lexbuf
+    tokens
 
-     "ab cdef ghijklmno12345",
-        [IDENT("ab"); IDENT("cdef"); IDENT("ghijklmno12345"); EOF];
-    
-    ] |> List.map (fun (a, b) -> TestCaseData(a).Returns(b))
+let correctIdentifierTestCases = 
+    [
+        // just small letters
+        "id",
+        [IDENT("id")];
+
+        // just capital letters
+        "IDENTIFIER",
+        [IDENT("IDENTIFIER")];
+
+        // mix of small and uppder with multiple whitespaces
+        "aBcDeFgHi          JkLmNoP",
+        [IDENT("aBcDeFgHi"); IDENT("JkLmNoP")];
+
+        // mix of small and uppder and digits with multiple whitespaces
+        "aBcDeFgHi1          JkLmNoP23  r098765",
+        [IDENT("aBcDeFgHi1"); IDENT("JkLmNoP23"); IDENT("r098765")];
+
+        // few identifiers in a row
+        "ab cdef",
+        [IDENT("ab"); IDENT("cdef")];
+
+        // few more identifires in a row
+        "ab cdef ghijklmno12345",
+        [IDENT("ab"); IDENT("cdef"); IDENT("ghijklmno12345")];
+
+    ] |> List.map (fun (a, b) -> TestCaseData(a).Returns(b @ [EOF]))
+
+let correctPunctuationsTestCases = 
+    [
+        // recognizes commas
+        ", , ,            ,  ,",
+        [COMMA; COMMA; COMMA; COMMA; COMMA];
+
+        // recognizes colons
+        ":  : :      :   : :",
+        [COLON; COLON; COLON; COLON; COLON; COLON];
+
+        // recognizes semicolons
+        ";     ; ;",
+        [SEMICOLON; SEMICOLON; SEMICOLON];
+
+        // recognizes left and right parens
+        "( )) (( )",
+        [LPAREN; RPAREN; RPAREN; LPAREN; LPAREN; RPAREN];
+
+    ] |> List.map (fun (a, b) -> TestCaseData(a).Returns(b @ [EOF]))
 
---
+++ src/CuddlyUmbrella.Compiler/ParserUtils.fs
@@ -0,0 +1,30 @@
+﻿module CuddlyUmbrella.Compiler.ParserUtils
+
+open Parser
+
+let private keywords_table =
+    [
+        "def", DEF;
+        "return", RETURN;
+
+    ] |> Map.ofList
+
+let private punctuations_table =
+    [
+        ",", COMMA;
+        ":", COLON;
+        ";", SEMICOLON;
+        "(", LPAREN;
+        ")", RPAREN;
+
+    ] |> Map.ofList
+
+let keywordOrIdentifierToken lexeme =
+    match keywords_table.TryFind(lexeme) with   
+    | Some(token) -> token   
+    | None -> IDENT(lexeme)
+
+let punctuationToken lexeme =
+    match punctuations_table.TryFind(lexeme) with   
+    | Some(token) -> token   
+    | None -> failwith ("uknown punctuation: " + lexeme)
